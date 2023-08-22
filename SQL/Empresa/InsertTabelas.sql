ebf4c9fed22d2c3a897217e362877613d7e5d94c
henry <127354681+henryzitos@users.noreply.github.com>
2023-08-21 21:23:43 -0300

    Create InsertTabelas.sql

---
+++ SQL/Empresa/InsertTabelas.sql
@@ -0,0 +1,6 @@
+INSERT INTO cargo (id_cargo, Nome_cargo) VALUES (1, 'CEO');
+INSERT INTO cargo (id_cargo, Nome_cargo) VALUES (2, 'Patrão');
+INSERT INTO cargo (id_cargo, Nome_cargo) VALUES (3, 'Gerente');
+INSERT INTO cargo (id_cargo, Nome_cargo) VALUES (4, 'Vendedor');
+INSERT INTO cargo (id_cargo, Nome_cargo) VALUES (5, 'Auxiliar de escritório');
+
