390ea627603392bb6f241675e41c6e53082fdc9d
henry <127354681+henryzitos@users.noreply.github.com>
2023-08-30 14:43:31 -0300

    Create AlteraEmpregado.sql

---
+++ SQL/Empresa/AlteraSexoeData.sql
@@ -0,0 +1,121 @@
+ALTER TABLE Empregado ADD sexo VARCHAR(10), ADD dataNascimento DATE;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 1;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 2;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 3;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 4;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 5;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 6;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 7;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 8;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 9;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 10;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 11;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 12;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 13;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 14;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 15;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 16;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 17;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 18;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 19;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 20;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 21;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 22;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 23;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 24;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 25;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 26;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 27;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 28;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 29;
+
+UPDATE Empregado
+SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+WHERE Matricula = 30;
