1d4e785aafbbb16281bc3c31c867246e91e92bdc
henry <127354681+henryzitos@users.noreply.github.com>
2023-08-31 20:22:41 -0300

    Update

--- SQL/Empresa/4Select.sql
+++ SQL/Empresa/4Select.sql
@@ -1,6 +1,54 @@
+-- TODOS OS EMPREGADOS
 SELECT * FROM Empregado;
+-- TODOS OS DEPENDENTES
 SELECT * FROM Dependente;
-SELECT * FROM Orgao;
+-- TODOS OS CARGOS
+SELECT * FROM Cargo;
+
+-- TODOS OS ÓRGÃOS E SEUS CARGOS
+SELECT o.Nome_orgao, c.Nome_cargo FROM Orgao o 
+INNER JOIN Empregado e ON o.id_orgao = e.fk_Orgao_id_orgao 
+INNER JOIN Cargo c ON e.fk_Cargo_id_cargo = c.id_cargo 
+GROUP BY o.Nome_orgao, c.Nome_cargo;
+-- TODOS OS EMPREGADOS E SEUS DEPENDENTES
+SELECT DISTINCT e.Nome FROM Empregado e INNER JOIN Dependente d ON e.Matricula = d.fk_Empregado_Matricula;
+-- TODOS OS EMPREGADOS SEM DEPENDENTES
+SELECT e.Nome
+FROM Empregado e
+WHERE e.Matricula NOT IN (
+  SELECT DISTINCT d.fk_Empregado_Matricula
+  FROM Dependente d
+  );
+
+-- TODOS OS CARGOS SEM EMPREGADOS CADASTRADOS
+SELECT C.Nome_cargo
+FROM Cargo C
+WHERE C.id_cargo NOT IN (
+    SELECT DISTINCT E.fk_Cargo_id_cargo
+    FROM Empregado E
+);
+-- TODOS OS ÓRGÃOS SEM CARGOS
+SELECT O.Nome_orgao
+FROM Orgao O
+WHERE O.id_orgao NOT IN (
+    SELECT DISTINCT E.fk_Orgao_id_orgao
+    FROM Empregado E
+);
+
+MÉDIA DOS SALÁRIOS POR CARGO E ÓRGÃO
+-- MÉDIA DOS SALÁRIOS
+SELECT AVG(Salario) FROM Empregado;
+-- MÉDIA DOS SALÁRIOS POR CARGO
+SELECT C.Nome_cargo, AVG(E.Salario)
+FROM Cargo C
+INNER JOIN Empregado E ON C.id_cargo = E.fk_Cargo_id_cargo
+GROUP BY C.Nome_cargo;
+-- MÉDIA DOS SALÁRIOS POR CARGO E ÓRGÃO
+SELECT Orgao.Nome_orgao, Cargo.Nome_cargo, AVG(Empregado.Salario)
+FROM Orgao
+INNER JOIN Empregado ON Orgao.id_orgao = Empregado.fk_Orgao_id_orgao
+INNER JOIN Cargo ON Empregado.fk_Cargo_id_cargo = Cargo.id_cargo
+GROUP BY Orgao.Nome_orgao, Cargo.Nome_cargo;
 
 -- SOMA DOS SALÁRIOS
 SELECT sum(Salario) FROM Empregado;
