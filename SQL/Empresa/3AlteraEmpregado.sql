095679c316798b6789847fcdeb33e4396e982a72
henry <127354681+henryzitos@users.noreply.github.com>
2023-08-31 14:03:21 -0300

    Update 3AlteraEmpregado.sql

--- SQL/Empresa/3AlteraEmpregado.sql
+++ SQL/Empresa/3AlteraEmpregado.sql
@@ -1,121 +1,121 @@
 ALTER TABLE Empregado ADD sexo VARCHAR(10), ADD dataNascimento DATE;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2006-10-28'
 WHERE Matricula = 1;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2007-04-10'
 WHERE Matricula = 2;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2007-05-26'
 WHERE Matricula = 3;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2006-09-16'
 WHERE Matricula = 4;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2006-08-31'
 WHERE Matricula = 5;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2007-01-11'
 WHERE Matricula = 6;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2006-08-18'
 WHERE Matricula = 7;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2003-08-15'
 WHERE Matricula = 8;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2001-12-03'
 WHERE Matricula = 9;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2002-05-28'
 WHERE Matricula = 10;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2004-09-19'
 WHERE Matricula = 11;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2001-11-10'
 WHERE Matricula = 12;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2003-04-22'
 WHERE Matricula = 13;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2002-08-07'
 WHERE Matricula = 14;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2000-02-14'
 WHERE Matricula = 15;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2001-06-09'
 WHERE Matricula = 16;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2004-10-26'
 WHERE Matricula = 17;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2002-03-18'
 WHERE Matricula = 18;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2004-01-30'
 WHERE Matricula = 19;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2001-06-12'
 WHERE Matricula = 20;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2002-09-08'
 WHERE Matricula = 21;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2004-11-04'
 WHERE Matricula = 22;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2005-12-21'
 WHERE Matricula = 23;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2001-05-02'
 WHERE Matricula = 24;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2003-08-25'
 WHERE Matricula = 25;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2004-07-06'
 WHERE Matricula = 26;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2000-09-05'
 WHERE Matricula = 27;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2002-03-29'
 WHERE Matricula = 28;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Feminino', dataNascimento = '2004-10-17'
 WHERE Matricula = 29;
 
 UPDATE Empregado
-SET sexo = 'Masculino', dataNascimento = '1980-01-01'
+SET sexo = 'Masculino', dataNascimento = '2003-11-01'
 WHERE Matricula = 30;
